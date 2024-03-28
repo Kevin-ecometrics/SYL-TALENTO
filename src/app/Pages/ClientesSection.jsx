e6b3612b0bea6c53b8947f274c8750f497d91655
Superuser <kevin@e-commetrics.com>
2024-03-28 14:39:10 -0700

    servicios actualizados

--- src/app/Pages/ClientesSection.jsx
+++ src/app/Pages/ClientesSection.jsx
@@ -25,6 +25,7 @@ function ClientesSection() {
     "/Silza trabaja con syl talento.png",
     "/sds de mexico con syl talento.svg",
     "/us comtech advanced con syl talento.svg",
+    "/enerser con syltalento.png",
   ];
 
   let tabs = [
@@ -32,50 +33,110 @@ function ClientesSection() {
       id: "Todos",
       label: "Nuestros Clientes",
       images: allImages,
-      alt: ["Client logo"]
+      alt: ["Client logo"],
     },
     {
       id: "Electronica",
       label: "Electronica",
-      images: ["/Benchmark trabaja con Syl talento.svg", "/Ricoh trabaja con Syl talento.svg", "/Honeywell trabaja con syltalento.svg", "/Safariland trabaja con Syl talento.svg", "/Integer trabaja con syl talento.svg", "/CM PARTNER TRABAJA CON SYL TALENTO.png", "/us comtech advanced con syl talento.svg"]
-      ,alt: ["Benchmark trabaja con Syl talento.svg", "Ricoh trabaja con Syl talento.svg", "Honeywell trabaja con syltalento.svg", "Safariland trabaja con Syl talento.svg", "Integer trabaja con syl talento.svg", "CM PARTNER TRABAJA CON SYL TALENTO.png", "us comtech advanced con syl talento.svg"]
+      images: [
+        "/Benchmark trabaja con Syl talento.svg",
+        "/Ricoh trabaja con Syl talento.svg",
+        "/Honeywell trabaja con syltalento.svg",
+        "/Safariland trabaja con Syl talento.svg",
+        "/Integer trabaja con syl talento.svg",
+        "/CM PARTNER TRABAJA CON SYL TALENTO.png",
+        "/us comtech advanced con syl talento.svg",
+      ],
+      alt: [
+        "Benchmark trabaja con Syl talento.svg",
+        "Ricoh trabaja con Syl talento.svg",
+        "Honeywell trabaja con syltalento.svg",
+        "Safariland trabaja con Syl talento.svg",
+        "Integer trabaja con syl talento.svg",
+        "CM PARTNER TRABAJA CON SYL TALENTO.png",
+        "us comtech advanced con syl talento.svg",
+      ],
     },
     {
       id: "Medica",
       label: "Medica",
-      images: ["/CardinalHealth trabaja con Syl talento.svg", "/Integer trabaja con syl talento.svg", "sds de mexico con syl talento.svg", "/Fisher&oaykel trabaja con syl talento.svg"],
-      alt: ["CardinalHealth trabaja con Syl talento.svg", "Integer trabaja con syl talento.svg", "sds de mexico con syl talento.svg", "Fisher&oaykel trabaja con syl talento.svg"]
+      images: [
+        "/CardinalHealth trabaja con Syl talento.svg",
+        "/Integer trabaja con syl talento.svg",
+        "sds de mexico con syl talento.svg",
+        "/Fisher&oaykel trabaja con syl talento.svg",
+      ],
+      alt: [
+        "CardinalHealth trabaja con Syl talento.svg",
+        "Integer trabaja con syl talento.svg",
+        "sds de mexico con syl talento.svg",
+        "Fisher&oaykel trabaja con syl talento.svg",
+      ],
     },
     {
       id: "Energeticos",
       label: "Energeticos",
-      images: ["/Arco junto con syl talento.svg", "/Silza trabaja con syl talento.png"],
-      alt: ["Arco junto con syl talento.svg", "Silza trabaja con syl talento.png"]
+      images: [
+        "/Arco junto con syl talento.svg",
+        "/Silza trabaja con syl talento.png",
+      ],
+      alt: [
+        "Arco junto con syl talento.svg",
+        "Silza trabaja con syl talento.png",
+      ],
     },
     {
       id: "Aeroespacial",
       label: "Aeroespacial",
       images: ["/Eaton trabaja junto a syl talento.svg"],
-      alt: ["Eaton trabaja junto a syl talento.svg"]
+      alt: ["Eaton trabaja junto a syl talento.svg"],
     },
     {
       id: "Cedis y Logistica",
       label: "Cedis y Logistica",
-      images: ["/Oxxo cedis trabaja con syl talento.svg", "/Ceva trabaja con Syl talento.svg"],
-      alt: ["Oxxo cedis trabaja con syl talento.svg", "Ceva trabaja con Syl talento.svg"]
+      images: [
+        "/Oxxo cedis trabaja con syl talento.svg",
+        "/Ceva trabaja con Syl talento.svg",
+      ],
+      alt: [
+        "Oxxo cedis trabaja con syl talento.svg",
+        "Ceva trabaja con Syl talento.svg",
+      ],
     },
     {
       id: "Costura y Tapiceria",
       label: "Costura y Tapiceria",
-      images: ["/Safariland trabaja con Syl talento.svg", "/Calinor trabaja con Syl talento.svg", "/maclin con syl talento.png" ,"/Jacuzzi trabaja con Syl talento.png"],
-      alt: ["Safariland trabaja con Syl talento.svg", "Calinor trabaja con Syl talento.svg", "maclin con syl talento.png" ,"Jacuzzi trabaja con Syl talento.png"]
+      images: [
+        "/Safariland trabaja con Syl talento.svg",
+        "/Calinor trabaja con Syl talento.svg",
+        "/maclin con syl talento.png",
+        "/Jacuzzi trabaja con Syl talento.png",
+      ],
+      alt: [
+        "Safariland trabaja con Syl talento.svg",
+        "Calinor trabaja con Syl talento.svg",
+        "maclin con syl talento.png",
+        "Jacuzzi trabaja con Syl talento.png",
+      ],
     },
     {
       id: "Servicios y Conexos",
       label: "Servicios y Conexos",
-      images: ["/Arco junto con syl talento.svg", "/Cemex trabaja con syl talento.svg", "/Silza trabaja con syl talento.png"],
-      alt:["Arco junto con syl talento.svg", "Cemex trabaja con syl talento.svg", "Silza trabaja con syl talento.png"]
-    }
+      images: [
+        "/Arco junto con syl talento.svg",
+        "/Cemex trabaja con syl talento.svg",
+        "/Silza trabaja con syl talento.png",
+        "/enerser con syltalento.png",
+        "/linde trabaja con syl talento.svg",
+      ],
+      alt: [
+        "Arco junto con syl talento.svg",
+        "Cemex trabaja con syl talento.svg",
+        "Silza trabaja con syl talento.png",
+        "enerser con syltalento.png",
+        "linde trabaja con syl talento.svg",
+      ],
+    },
   ];
 
   return (
@@ -88,21 +149,32 @@ function ClientesSection() {
           Clientes
         </h1>
         <section className="flex flex-col items-center gap-4">
-        <Tabs aria-label="Dynamic tabs" items={tabs} className="hidden md:block">
-        {(item) => (
-          <Tab key={item.id} title={item.label}>
-            <Card>
-              <CardBody>
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                  {item.images.map((image, index) => (
-                    <Image key={index} src={image} width={160} height={80} alt={item.alt} title={item.alt} />
-                  ))}
-                </div>
-              </CardBody>
-            </Card>  
-          </Tab>
-        )}
-      </Tabs>
+          <Tabs
+            aria-label="Dynamic tabs"
+            items={tabs}
+            className="hidden md:block"
+          >
+            {(item) => (
+              <Tab key={item.id} title={item.label}>
+                <Card>
+                  <CardBody>
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                      {item.images.map((image, index) => (
+                        <Image
+                          key={index}
+                          src={image}
+                          width={160}
+                          height={80}
+                          alt={item.alt}
+                          title={item.alt}
+                        />
+                      ))}
+                    </div>
+                  </CardBody>
+                </Card>
+              </Tab>
+            )}
+          </Tabs>
         </section>
       </main>
     </div>
